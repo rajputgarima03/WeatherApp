b38a49f88b51083a2e75244f6ed587621bc26a06
GarimaRajput18 <garima.rajput@krazy-tech.com>
2024-12-11 23:24:36 +0530

    explicitly adding thunk

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import weatherReducer from './reducers/weatherReducers';
 const store = configureStore({
     reducer: {
         weather: weatherReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            thunk: true, 
+        }),
 });
 
 export default store;
